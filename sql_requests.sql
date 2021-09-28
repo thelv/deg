89b8bfad662fec2e65e428f0be49f0f2fe1d0f49
thelv <thelvs@gmail.com>
2021-09-28 03:38:45 +0300

    next

--- sql_requests.sql
+++ sql_requests.sql
@@ -1,3 +1,4 @@
+--создаём таблицу, в которой видно к ID избирателя связывается с порядковым номеров блока регистрации, чтобы потом увидеть "аномальные блоки", избиратели из которых странно голосуют
 drop table if exists voters;
 create table 
 	voters
@@ -36,6 +37,7 @@ as
 );
 create index on voters (voter_id);
 
+--создаем таблицу, в которой видно в какое время какой избиратель получил бюллетень, по этой и предыдущей таблице мы можем связать полученные бюллетени с блоками регистрации
 drop table if exists ballots;
 create table 
 	ballots
@@ -48,13 +50,10 @@ from
 where 
 	method_id=4 
 order by 
-	datetime
+	datetime;
 	
-	
-	
-	
---select count(*) from decrypted_ballots
---select count(*) from transactions where method_id=6
+--наша главная таблица (создание займет минут 30), в ней видны все акты голосование, т.е. когда, в каком округе и за кого была каждая бюллетень
+drop table if exists votes;
 create table 
 	votes 
 as select 
@@ -82,9 +81,10 @@ from
 group by 
 	district_id,
 	choise_id, 
-	((extract(epoch from datetime))/60)::int
+	((extract(epoch from datetime))/60)::int;
 	
-	
+
+--группируем голоса из предыдушей таблице по 6 минут
 drop table if exists votes_by_mins6;
 create table 
 	votes_by_mins6
@@ -98,8 +98,9 @@ from
 group by 
 	district_id,
 	choise_id, 
-	((extract(epoch from datetime))/360)::int
-	
+	((extract(epoch from datetime))/360)::int;
+
+--группируем голоса из предыдушей таблице по 30 минут	
 drop table if exists votes_by_mins30;
 create table 
 	votes_by_mins30
@@ -113,11 +114,9 @@ from
 group by 
 	district_id,
 	choise_id, 
-	((extract(epoch from datetime))/1800)::int
-	
-	
-	
---------------
+	((extract(epoch from datetime))/1800)::int;
+
+--запрос для того, чтобы определить за определенный период времени, люди из каких блоков регистрации больше голосовали (а точнее не голосовали, а получили бюллетень, т.к. сам акт голосования не привязан к ID избирателя)
 
 select 
 	count(*), (transaction_order/10)::int
@@ -130,11 +129,9 @@ using
 	voter_id
 )
 where 
-	b.datetime>'2021-09-17 4:00:33.02+03'::timestamptz and
-	b.datetime<'2021-09-17 22:00:33.02+03'::timestamptz
+	b.datetime>'2021-09-17 4:00:33.02+03'::timestamptz and --начало периода
+	b.datetime<'2021-09-17 22:00:33.02+03'::timestamptz --конец периода (подставьте что хотите)
 group by 
 	(v.transaction_order/10)::int
 order by 
-	(v.transaction_order/10)::int
-
-	
+	(v.transaction_order/10)::int;
